a92acc323d4d05815489b951cfbd053df960c5c9
Aniket Kumar <Aniketkr.kumar58@gmail.com>
2023-08-22 00:12:15 +0530

    api Start

---
+++ backend/api/controllers/adminController.js
@@ -0,0 +1,21 @@
+import adminModels from '../models/adminModels.js';
+
+export const adminRegisterController = async (req, res, next) => {
+    const { name, email, phone, password } = req.body;
+    try {
+
+        /** Validation All Entites */
+        if (!name && !email && !phone && !password ) {
+            return result.status(403).write({ success: false, message: "All Data are required" })
+        }
+        console.log(name)
+        console.log(email)
+
+    } catch (error) {
+        console.log("Error in User Register Controller", error);
+        return res.status(500).json({
+            sucess: false,
+            msg: 'Internal Server Error'
+        })
+    }
+};
--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import connectDB from "./config/database/connection.js";
+import adminRoute from './api/routes/adminRoute.js'
 const app  = Router();
 
 
+app.use('api/v1/auth',adminRoute);
 
 connectDB(); 
 export default app;
